e4887fb8787469fab791ec85532facda845c416a
Karla Goo <karla_goo@yahoo.com>
2021-06-17 23:40:49 -0500

    made edits

--- script.js
+++ script.js
@@ -1,58 +1,33 @@
 
-function playRPS() {
+// Array of options for computer to pick from
+var options = ["R", "P", "S"]
 
-    window.prompt("Enter R, P, S");
-    let playerChoice = prompt(`Enter R, P, or S`);
-    let options= ["R", "P", "S"]
+var playRPS = function () {
+    // Ask player for choice
+    var playerChoice = window.prompt("Enter R, P or S:")
+    // If player chooses Cancel, end the game
+    if (!playerChoice) {
+        return;
+    }
 }
 
-// function declareHello() { 
-//     console.log("Hello, I am a function declaration.");
-//     console.log("-----------------------------------");
-//     // Return stops the execution of a function
-//     return;
-//   }
+    // // Convert to uppercase to make comparisons easier
+    // playerChoice = playerChoice.toUppperCase()
+    // // Ask about this: Doesn't this just capitalize all of the words in the options? Why?
+
+    // Here is the random function to get a random array of options
+    var index = Math.floor(Math.random() * options.length);
+    var computerChoice = options[index]
+    // Ask about the brackets on here: why not parenthesis? Is it just an array multiplied?
+
+    window.alert("The Computer chose " + computerChoice);
+
+    // if choices are the same, it's a tie
+    if (playerChoice === computerChoice) {
+        ties++;
+        window.alert("It's a tie!")
+    }
 
 
-// let computerChoice = Math.floor(Math.random() * options.length);
-let computerChoice = options[Math.floor(Math.random()*options.length)];
-// prompt[Math.floor(Math.random() * prompt.length)]
-
-if(
-    (computerChoice === playerChoice)
-    )
-{
-    alert("Tie, play again?");
-}    
 
 
-if (
-    ((playerChoice === "P" && computerChoice === "R") ||
-    (playerChoice === "S" && computerChoice === "P") ||
-    (playerChoice === "R" && computerChoice === "S"))
-    )
-    
-{
-alert("You win!");
-}
-
-    
-if (
-    ((computerChoice === "S" && playerChoice === "P") ||
-    (computerChoice === "R" && playerChoice === "S") ||
-    (computerChoice === "P" && playerChoice === "S"))
-    )
-{
-alert("You lose!")
-}
-// window.alert("The computer chose [random]");
-
-// window.alert("You ___! [either win, lose or tie against computer]");
-
-// window.alert("Stats: Wins: Losses: "); 
-
-if (window.confirm("Play again?")) {
-     open ()}
-else {
-    exit();
-}
